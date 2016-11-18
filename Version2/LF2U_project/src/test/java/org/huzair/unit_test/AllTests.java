38be3dfc79e7b69cf07f9c8cb2dd2b45ba3a2dd7
Huzair Kalia <Huzair Kalia>
2016-11-17 19:36:58 -0600

    Update

--- Version2/LF2U_project/src/test/java/org/huzair/unit_test/AllTests.java
+++ Version2/LF2U_project/src/test/java/org/huzair/unit_test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({CustomerTest.class, DeliveryTest.class, FarmersTest.class, ManagerTest.class, SearchTest.class,RESTManagerTest.class,RESTFarmerTest.class, ValidationTest.class})
+@SuiteClasses({CustomerTest.class, DeliveryTest.class, FarmersTest.class, ManagerTest.class, SearchTest.class,RESTManagerTest.class, ValidationTest.class})
 
 public class AllTests {
 
